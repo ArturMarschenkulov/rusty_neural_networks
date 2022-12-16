d357c2a3dbbd225be344cb443217ae5c55b4cab1
artur <ArturMarschals@web.de>
2022-12-16 09:55:38 +0100

    now it runs however there is no learning

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,172 @@
-mod matrix;
 mod t;
 
-use matrix::*;
+use ndarray as nd;
 
-fn relu(z: &Matrix) -> Vec<f64> {
-    let mut a: Vec<f64> = Vec::new();
-    // for i in 0..z.rows {
-    //     for j in 0..z.cols {
-    //         a.push(z.data[i][j].max(0.0));
-    //     }
-    // }
-    // let a: Vec<f64> = z.iter().map(|&z| z.max(0.0)).collect();
-    // let cache = z.to_vec();
-    a
-}
+fn gradient_descent(x: &nd::Array2<f64>, y: &nd::Array1<f64>, alpha: f64, iterations: usize) -> () {
+    let (mut w_1, mut b_1, mut w_2, mut b_2) = init_params();
 
-fn softmax(z: &[f64]) -> Vec<f64> {
-    let exp_z: Vec<f64> = z.iter().map(|&z| z.exp()).collect();
-    let sum_exp_z: f64 = exp_z.iter().sum();
-    exp_z.iter().map(|&z| z / sum_exp_z).collect()
-}
-
-fn transpose(x: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
-    let mut data_t: Vec<Vec<f64>> = Vec::new();
-    for i in 0..x[0].len() {
-        let mut row: Vec<f64> = Vec::new();
-        for j in 0..x.len() {
-            row.push(x[j][i]);
+    for i in 0..iterations {
+        let (z_1, a_1, z_2, a_2) = forward_prop(&w_1, &b_1, &w_2, &b_2, x);
+        let (dw_1, db_1, dw_2, db_2) = back_prop(&z_1, &a_1, &z_2, &a_2, &w_1, &w_2, x, y);
+        (w_1, b_1, w_2, b_2) =
+            update_params(&w_1, &b_1, &w_2, &b_2, &dw_1, &db_1, &dw_2, &db_2, alpha);
+        if i % 2 == 0 {
+            println!("Iteration: {}", i);
+            let prediction = get_predictions(&a_2);
+            println!("Accuracy: {}", get_accuracy(&prediction, y));
         }
-        data_t.push(row);
     }
-    data_t
+    // (w_1, b_1, w_2, b_2)
 }
 
-fn transpose_(x: &Vec<MnistPic>) -> Vec<Vec<f64>> {
-    let mut data_t: Vec<Vec<f64>> = Vec::new();
-    for i in 0..x[0].pixels.len() {
-        let mut row: Vec<f64> = Vec::new();
-        for j in 0..x.len() {
-            row.push(x[j].pixels[i]);
+fn argmax(a: &nd::Array2<f64>) -> nd::Array1<f64> {
+    let mut res = nd::Array1::zeros(a.len_of(nd::Axis(1)));
+    for (i, j) in a.genrows().into_iter().enumerate() {
+        let mut max = 0.0;
+        let mut max_index = 0;
+        for (k, l) in j.iter().enumerate() {
+            if *l > max {
+                max = *l;
+                max_index = k;
+            }
         }
-        data_t.push(row);
+        res[i] = max_index as f64;
     }
-    data_t
+    res
 }
 
+fn get_predictions(a_2: &nd::Array2<f64>) -> nd::Array1<f64> {
+    argmax(&a_2)
+}
+fn get_accuracy(predictions: &nd::Array1<f64>, y: &nd::Array1<f64>) -> f64 {
+    // give an array of true and false
+    let bools = predictions
+        .iter()
+        .zip(y.iter())
+        .map(|(x, y)| x == y)
+        .collect::<Vec<_>>();
+
+    // count the number of true
+    let true_count = bools.iter().filter(|x| **x).count();
+    true_count as f64 / y.len() as f64
+}
+fn forward_prop(
+    w_1: &nd::Array2<f64>,
+    b_1: &nd::Array2<f64>,
+    w_2: &nd::Array2<f64>,
+    b_2: &nd::Array2<f64>,
+    x: &nd::Array2<f64>,
+) -> (
+    nd::Array2<f64>,
+    nd::Array2<f64>,
+    nd::Array2<f64>,
+    nd::Array2<f64>,
+) {
+    let z_1 = w_1.dot(x) + b_1;
+    let a_1 = z_1.map(|x| relu(*x));
+    let z_2 = w_2.dot(&a_1) + b_2;
+    let a_2 = z_2.map(|x| relu(*x));
+    (z_1, a_1, z_2, a_2)
+}
+
+fn back_prop(
+    z_1: &nd::Array2<f64>,
+    a_1: &nd::Array2<f64>,
+    z_2: &nd::Array2<f64>,
+    a_2: &nd::Array2<f64>,
+    w_1: &nd::Array2<f64>,
+    w_2: &nd::Array2<f64>,
+    x: &nd::Array2<f64>,
+    y: &nd::Array1<f64>,
+) -> (nd::Array2<f64>, f64, nd::Array2<f64>, f64) {
+    let m = 785.0;
+    let one_hot_y = one_hot(y);
+    let dz_2 = a_2 - one_hot_y;
+    let dw_2 = dz_2.dot(&a_1.t()) / m as f64;
+    let db_2 = dz_2.sum() / m;
+
+    let dz_1 = w_2.t().dot(&dz_2).map(|x| relu_deriv(*x));
+    let dw_1 = dz_1.dot(&x.t()) / m as f64;
+    let db_1 = dz_1.sum() / m;
+    (dw_1, db_1, dw_2, db_2)
+}
+fn update_params(
+    w_1: &nd::Array2<f64>,
+    b_1: &nd::Array2<f64>,
+    w_2: &nd::Array2<f64>,
+    b_2: &nd::Array2<f64>,
+    dw_1: &nd::Array2<f64>,
+    db_1: &f64,
+    dw_2: &nd::Array2<f64>,
+    db_2: &f64,
+    alpha: f64,
+) -> (
+    nd::Array2<f64>,
+    nd::Array2<f64>,
+    nd::Array2<f64>,
+    nd::Array2<f64>,
+) {
+    let w_1 = w_1 - dw_1 * alpha;
+    let b_1 = b_1 - db_1 * alpha;
+    let w_2 = w_2 - dw_2 * alpha;
+    let b_2 = b_2 - db_2 * alpha;
+    (w_1, b_1, w_2, b_2)
+}
+
+fn one_hot(y: &nd::Array1<f64>) -> nd::Array2<f64> {
+    let mut one_hot_y = nd::Array2::zeros((10, y.len_of(nd::Axis(0))));
+    for (i, j) in y.iter().enumerate() {
+        one_hot_y[[*j as usize, i]] = 1.0;
+    }
+    one_hot_y
+}
+
+fn relu(x: f64) -> f64 {
+    if x > 0.0 {
+        x
+    } else {
+        0.0
+    }
+}
+fn relu_deriv(x: f64) -> f64 {
+    if x > 0.0 {
+        1.0
+    } else {
+        0.0
+    }
+}
+#[derive(Debug, Clone)]
+struct MnistPic {
+    label: u8,
+    pixels: Vec<f64>,
+}
+impl MnistPic {
+    fn new(label: u8, pixels: &Vec<f64>) -> Self {
+        assert_eq!(pixels.len(), 784, "Pixel vector must be 784 long");
+        MnistPic {
+            label,
+            pixels: pixels.clone(),
+        }
+    }
+}
+
+fn init_params() -> (
+    nd::Array2<f64>,
+    nd::Array2<f64>,
+    nd::Array2<f64>,
+    nd::Array2<f64>,
+) {
+    use rand::Rng;
+
+    let mut rng = rand::thread_rng();
+    let range = -0.5..=0.5;
+
+    let w_1 = nd::Array2::<f64>::zeros((10, 784)).map(|_| rng.gen_range(range.clone()));
+    let b_1 = nd::Array2::<f64>::zeros((10, 1)).map(|_| rng.gen_range(range.clone()));
+    let w_2 = nd::Array2::<f64>::zeros((10, 10)).map(|_| rng.gen_range(range.clone()));
+    let b_2 = nd::Array2::<f64>::zeros((10, 1)).map(|_| rng.gen_range(range.clone()));
+    (w_1, b_1, w_2, b_2)
+}
 fn to_data(records: Vec<csv::StringRecord>) -> Vec<Vec<f64>> {
     let mut res: Vec<Vec<f64>> = Vec::new();
     for i in 0..records.len() {
@@ -64,83 +186,6 @@ fn to_data(records: Vec<csv::StringRecord>) -> Vec<Vec<f64>> {
                 .collect()
         })
         .collect()
-}
-fn forward_prop(
-    w_1: &Matrix,
-    b_1: &Matrix,
-    w_2: &Matrix,
-    b_2: &Matrix,
-    x: &Matrix,
-) -> (Vec<f64>, Vec<f64>, Vec<f64>, Vec<f64>) {
-    // don't use outside libraries
-    // Z1 = W1.dot(X) + b1
-    let z_1: Matrix = w_1.dot_multiply(x).add(b_1);
-    assert_eq!(z_1.rows, w_1.rows);
-    assert_eq!(z_1.cols, x.cols);
-    let a_1 = relu(&z_1);
-    unimplemented!()
-}
-
-fn gradient_descent(x: &Matrix, y: &Vec<f64>, alpha: f64, iterations: usize) {
-    let (w_1, b_1, w_2, b_2) = init_params();
-
-    // for i in 0..iterations {
-    //     let (z_1, a_1, z_2, a_2) = forward_prop(x, &w_1, &b_1, &w_2, &b_2);
-    //     let (dz_2, dw_2, db_2, dz_1, dw_1, db_1) = back_prop(x, y, &z_1, &a_1, &z_2, &a_2);
-    //     let (w_1, b_1, w_2, b_2) =
-    //         update_params(&w_1, &b_1, &w_2, &b_2, &dw_1, &db_1, &dw_2, &db_2, alpha);
-    // }
-}
-#[derive(Debug, Clone)]
-struct MnistPic {
-    label: u8,
-    pixels: Vec<f64>,
-}
-impl MnistPic {
-    fn new(label: u8, pixels: &Vec<f64>) -> Self {
-        assert_eq!(pixels.len(), 784, "Pixel vector must be 784 long");
-        MnistPic {
-            label,
-            pixels: pixels.clone(),
-        }
-    }
-}
-
-fn init_params() -> (Matrix, Matrix, Matrix, Matrix) {
-    use rand::Rng;
-
-    let mut rng = rand::thread_rng();
-    let range = -0.5..=0.5;
-
-    let w_1: Matrix = Matrix::new(
-        10,
-        784,
-        (0..=10)
-            .map(|_| (0..=784).map(|_| rng.gen_range(range.clone())).collect())
-            .collect::<Vec<Vec<f64>>>(),
-    );
-    let b_1: Matrix = Matrix::new(
-        10,
-        1,
-        (0..=10)
-            .map(|_| (0..=1).map(|_| rng.gen_range(range.clone())).collect())
-            .collect::<Vec<Vec<f64>>>(),
-    );
-    let w_2 = Matrix::new(
-        10,
-        10,
-        (0..=10)
-            .map(|_| (0..=10).map(|_| rng.gen_range(range.clone())).collect())
-            .collect::<Vec<Vec<f64>>>(),
-    );
-    let b_2 = Matrix::new(
-        10,
-        1,
-        (0..=10)
-            .map(|_| (0..=1).map(|_| rng.gen_range(range.clone())).collect())
-            .collect::<Vec<Vec<f64>>>(),
-    );
-    (w_1, b_1, w_2, b_2)
 }
 
 fn get_mnist_data() -> Vec<MnistPic> {
@@ -170,50 +215,53 @@ fn get_mnist_data_2() -> ndarray::Array2<f64> {
     let file = File::open("mnist_test.csv").unwrap();
     let mut rdr = csv::Reader::from_reader(BufReader::new(file));
 
-    let data = rdr
+    let records = rdr
         .records()
         .map(|result| result.unwrap())
         .collect::<Vec<_>>();
 
-    let data = to_data(data);
-    let mnist_pics = data
-        .iter()
-        .map(|d| MnistPic::new(d[0] as u8, &d[1..].to_vec()))
-        .collect::<Vec<_>>();
+    // let data = to_data(data);
+    // let mnist_pics = data
+    //     .iter()
+    //     .map(|d| MnistPic::new(d[0] as u8, &d[1..].to_vec()))
+    //     .collect::<Vec<_>>();
 
-    let mut res = ndarray::Array2::<f64>::zeros((mnist_pics.len(), 784));
-    for (i, pic) in mnist_pics.iter().enumerate() {
-        for (j, pixel) in pic.pixels.iter().enumerate() {
-            res[[i, j]] = *pixel;
+    let mut res = ndarray::Array2::<f64>::zeros((records.len(), 785));
+    for (i, record) in records.iter().enumerate() {
+        for (j, pixel) in record.iter().enumerate() {
+            res[[i, j]] = pixel.parse::<f64>().unwrap();
         }
     }
     res
 }
 
-
 fn main() {
-    // t::main();
-    // return;
-    use ndarray as nd;
+    // Enable RUST_BACKTRACE=1 to see the backtrace
+    std::env::set_var("RUST_BACKTRACE", "1");
 
     let mnist_pics = get_mnist_data_2();
+    println!("mnist_pics: {:?}", mnist_pics);
+    println!("mnist_pics: {:?}", mnist_pics.slice(nd::s![..=0, ..]));
     let dev_size: i32 = 1000;
-
-    let ss: nd::Array2<f32> = nd::array!([1., 2., 4.0], [6., 9., 2.3], [4., 6., 9.9]);
-    println!("{:?}", ss);
-    let ss = ss.slice(nd::s![..=0, ..]);
-    println!("{:?}", ss);
 
     // slice `mnist_pics` from 0 to 1000
     let mnist_pics_test = mnist_pics.slice(nd::s![0..dev_size, ..]);
     let data_test = mnist_pics_test.t();
-    let y_test = data_test.slice(nd::s![..=0, ..]);
-    let x_test = data_test.slice(nd::s![1..1000, ..]);
+
+    let y_test = data_test.slice(nd::s![0, ..]);
+
+    println!("data_test: {:?}", data_test);
+    println!("y_test: {:?}", y_test);
+    let x_test = data_test.slice(nd::s![1..785, ..]);
+    println!("x_test: {:?}", x_test);
     let x_test = x_test.map(|x| *x / 255.0);
 
     let mnist_pics_train = mnist_pics.slice(nd::s![dev_size.., ..]);
     let data_train = mnist_pics_train.t();
-    let y_train = data_test.slice(nd::s![..=0, ..]);
-    let x_train = data_test.slice(nd::s![1..1000, ..]);
+    let y_train = data_train.slice(nd::s![0, ..]);
+    let x_train = data_train.slice(nd::s![1..785, ..]);
     let x_train = x_train.map(|x| *x / 255.0);
+
+    println!("y_train: {:?}", y_train);
+    gradient_descent(&x_train, &y_train.to_owned(), 0.10, 5000);
 }
